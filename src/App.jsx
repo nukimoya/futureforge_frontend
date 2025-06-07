3e49ca68e346d358f576c4a13d1d5643d2ae3816
Samuel Adelakun <93160015+nukimoya@users.noreply.github.com>
2025-06-07 05:46:18 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">🚀 Tailwind is working!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
