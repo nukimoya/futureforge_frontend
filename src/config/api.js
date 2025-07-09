7078616c62adb4ec98c87e947d261fcefe98f949
Samuel Adelakun <sayomikun123@gmail.com>
2025-07-09 09:02:02 -0700

    Changing BaseUrl again

--- src/config/api.js
+++ src/config/api.js
@@ -10,7 +10,7 @@ export const useAxios = () => {
 
   const api = useMemo(() => {
     const instance = axios.create({
-      baseURL: 'ashamed-loren-samuel-dev-8703a6f6.koyeb.app/',
+      baseURL: 'https://ashamed-loren-samuel-dev-8703a6f6.koyeb.app/',
       timeout: 5000,
       withCredentials: true, 
       headers: {
